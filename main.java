a777f6c395210cdd334b285b850c5eb4f4aed265
ShuvoRahmanShuv <shuvorahmanshuv@gmail.com>
2025-05-12 13:21:42 +0600

    Person Java

--- main.java
+++ main.java
@@ -8,13 +8,17 @@ class Person {
     }
 
     public void Work() {
-        System.out.println("Works");
+        System.out.println(Pname + " Works");
     }
 }
 public class main {
     public static void main(String[] args) {
         Person person1 = new Person("Rahim", 18);
         Person person2 = new Person("Karim", 20);
-        System.out.println(person1.Pname + " " + person1.P_age + "\n" + person2.Pname + " " + person2.P_age);
+
+        System.out.println(person1.Pname + " " + person1.P_age);
+        System.out.println(person2.Pname + " " + person2.P_age);
+
+        person2.Work(); 
     }
 }
